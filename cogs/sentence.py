06b8adbcc1e00c9c758f65cfe9cc672a29511ea5
Ritik Shah <ritik10.shah@gmail.com>
2018-11-06 14:31:20 -0800

    Completed sentence cog!
    New:
     Sentence cog fully working
     >sentence new <filename> - creates a new grammar file
     >sentence generate <filename> <symbol> - generates a random sentence

--- cogs/sentence.py
+++ cogs/sentence.py
@@ -2,6 +2,8 @@ import discord
 import logging
 import asyncio
 import os.path
+import random
+import json
 import re
 from discord.ext import commands
 
@@ -19,7 +21,7 @@ class SentenceCog:
 		with open(name + '.txt', 'r') as file:
 			return file.split('\n')
 
-	def create(self, filename: str, raw: str):
+	def create(self, raw: str):
 		grammar = {}
 
 		spaces = re.compile('\\s+')
@@ -27,6 +29,7 @@ class SentenceCog:
 
 		# Ensure every line has ':=='
 		for line in [ele.split('::=') for ele in raw.split('\n')]:
+			print(line)
 			assert(len(line) == 2)
 			assert(line[0] not in grammar)
 
@@ -49,18 +52,39 @@ class SentenceCog:
 		print('hello world')
 
 	@sentence.command(name="generate")
-	async def sentence_generate(self, ctx):
-		await ctx.send("")
+	async def sentence_generate(self, ctx, filename, symbol):
+		self.LOGGER.info('Sentence generate ran')
+		
+		grammar = {}
+
+		with open('cogs/grammar/' + filename + '.json', 'r') as file:
+			grammar = json.load(file)
+
+		await ctx.send(self.generate(symbol, grammar))
 
 	@sentence.command(name="new")
-	async def sentence_new(self, ctx, filename):
+	async def sentence_new(self, ctx):
 		self.LOGGER.info('Sentence new ran')
 
 		def check(msg):
 			return (msg.author == ctx.author) and (msg.guild is None)
 
+		def cancel(msg):
+			if msg.content.lower() in self.cancel_messages:
+				self.LOGGER.info('Command execution cancelled')
+				raise asyncio.TimeoutError
+
 		try:
 			lines = ''
+			await ctx.send("You have been dm'd with instructions.", delete_after=15.0)
+
+			await ctx.author.send('What would you like to name your grammar file? (Sending stop or cancel at any point will cancel this interaction).')
+			msg = await self.bot.wait_for('message', timeout=60.0, check=check)
+
+			cancel(msg)
+
+			filename = msg.content.lower()
+			self.LOGGER.info('Filename -> ' + filename)
 
 			# If file exists, output contents to user and proceed (if client wishes to)
 			if os.path.exists('cogs/grammar/' + filename + '.json'):
@@ -68,8 +92,6 @@ class SentenceCog:
 
 				with open('cogs/grammar/' + filename + '.json', 'r') as file:
 					lines = file.read()
-
-				await ctx.send("You have been dm'd with instructions.", delete_after=15.0)
 
 				await ctx.author.send('```' + lines + '```')
 				await ctx.author.send('Creating a new grammar file will override the above. Proceed? (Y/N)')
@@ -79,19 +101,21 @@ class SentenceCog:
 					self.LOGGER.info('Command execution cancelled')
 					raise asyncio.TimeoutError
 
+				cancel(msg)
+
 			# Makes new (or replaces) file w/ grammar
-			await ctx.author.send("Send me a message of your grammar file surrounded by \\`\\`\\`'s. Send stop or cancel to cancel the command.")
+			await ctx.author.send("Send me a message of your grammar file surrounded by \\`\\`\\`'s.")
 			msg = await self.bot.wait_for('message', timeout=60.0, check=check)
 
-			if msg.content.lower() in self.cancel_messages:
-				self.LOGGER.info('Command execution cancelled')
-				raise asyncio.TimeoutError
-
-			print(msg.content)
+			cancel(msg)
 
 			with open('cogs/grammar/' + filename + '.json', 'w') as outfile:
-				json.dump(create(msg.content.replace('```', '')), outfile)
+				dum = self.create(msg.content.replace('```', ''))
+				print(dum)
+				json.dump(dum, outfile)
 				self.LOGGER.info('Created file: ' + filename + '.json')
+
+			await ctx.author.send("Success!")
 
 			await ctx.send("Success! :white_check_mark:")
 
